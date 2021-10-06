9e652a3c7a6929cb7fa700dfc6aa11e3f9a51655
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-06 16:42:43 -0300

    CLI project to tests/compiled usage

---
+++ src/cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
