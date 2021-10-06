217c2bd7dd0a4ce7af7c6ad50bb7568baf39ca05
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-06 10:18:39 -0300

    Model update

---
+++ models/GitApiModels/GithubApiResonse.cs
@@ -0,0 +1,35 @@
+namespace GitGrabber.Models {
+    public class GithubApiResponse {
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+        public string current_user_url {get; set;}
+    }
+}
