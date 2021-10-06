ac868bea2311bfb2e033bf6475878f60d34e7da8
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-06 16:11:19 -0300

    Update GithubRepo/License Models

--- models/GitApiModels/GithubUserContent/GithubRepo.cs
+++ models/GitApiModels/GithubUserContent/GithubRepo.cs
@@ -34,49 +34,51 @@ namespace GitGrabber.Models {
         public string comments_url {get; set;}
         public string issue_comment_url {get; set;}
         public string contents_url {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
+        public string compare_url {get; set;}
+        public string merges_url {get; set;}
+        public string archive_url {get; set;}
+        public string downloads_url {get; set;}
+        public string issues_url {get; set;}
+        public string pulls_url {get; set;}
+        public string milestones_url {get; set;}
+        public string notifications_url {get; set;}
+        public string labels_url {get; set;}
+        public string releases_url {get; set;}
+        public string deployments_url {get; set;}
+        public string created_at {get; set;}
+        public string updated_at {get; set;}
+        public string pushed_at {get; set;}
+        public string git_url {get; set;}
+        public string ssh_url {get; set;}
+        public string clone_url {get; set;}
+        public string svn_url {get; set;}
+        public string homepage {get; set;}
+        public int size {get; set;}
+        public int stargazers_count {get; set;}
+        public int watchers_count {get; set;}
+        public string language {get; set;}
+        public bool has_issues {get; set;}
+        public bool has_projects {get; set;}
+        public bool has_downloads {get; set;}
+        public bool has_wiki {get; set;}
+        public bool has_pages {get; set;}
+        public int forks_count {get; set;}
+        public string mirror_url {get; set;}
+        public bool archived {get; set;}
 
-        public string node_id {get; set;}
+        public bool disabled {get; set;}
 
-        public string node_id {get; set;}
+        public int open_issues_count {get; set;}
 
-        public string node_id {get; set;}
+        public string license {get; set;} /* Info: Only the license key will be fetched from the requisition */
 
-        public string node_id {get; set;}
+        public bool allow_forking {get; set;}
 
-        public string node_id {get; set;}
+        public string visibility {get; set;}
 
-        public string node_id {get; set;}
-
+        public int forks {get; set;}
+        public int open_issues {get; set;}
+        public int watchers {get; set;}
+        public string default_branch {get; set;}
     }
 }
