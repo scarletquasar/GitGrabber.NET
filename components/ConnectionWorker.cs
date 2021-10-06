a93e8199742cca6c62e0b0073098de447eaf8bf8
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-06 10:36:07 -0300

    Update - General Environment

--- components/ConnectionWorker.cs
+++ components/ConnectionWorker.cs
@@ -1,5 +1,25 @@
+using GitGrabber.Models;
+using System.Net.NetworkInformation;
+
 namespace GitGrabber.Components {
-    public static class ConnectionWorkes {
-        public 
+    public static class ConnectionWorker {
+        public static GithubConnection Connect() {
+            try {
+                    var pinger = new Ping();
+                    PingReply reply = pinger.Send("api.github.com");
+                    var pingable = reply.Status == IPStatus.Success;
+                    var status = string.Empty;
+                    var latency = reply.RoundtripTime;
+                    
+                    return new GithubConnection() {
+                        connection_success = pingable,
+                        connection_latency = latency
+                    };
+            }
+            catch (PingException)
+            {
+                return new GithubConnection();
+            }
+        }
     }
 }
--- models/ConnectionWorker/GithubConnection.cs
+++ models/ConnectionWorker/GithubConnection.cs
@@ -1,6 +1,6 @@
 namespace GitGrabber.Models {
-    public class ConnectionWorker {
-        public string connection_status {get; set;}
-        public double connection_latency {get; set;}
+    public class GithubConnection {
+        public bool connection_success {get; set;} = false;
+        public long connection_latency {get; set;} = 0;
     }
 }
