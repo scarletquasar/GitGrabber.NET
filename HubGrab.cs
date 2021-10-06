cfc639dd39f08e4957c9112592bab8ed56c8c145
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-06 13:42:01 -0300

    Dev Update - Exception Dictionary

--- HubGrab.cs
+++ HubGrab.cs
@@ -10,17 +10,20 @@ namespace GitGrabber
             connection_success = false,
             connection_latency = 0
         };
+
+        /* Starts the connection. Method used to avoid different errors that can have different causes. */
         public bool Connect() {
             connection = ConnectionWorker.Connect();
             return connection.connection_success;
         }
 
+        /* Connects to the main api and gets a GithubApiResponse object with all the returns from the original API. */
         public GithubApiResponse GithubApi() {
             if(connection.connection_success) {
                 return ConnectionWorker.Fetch();
             }
             else {
-                return new GithubApiResponse();
+                throw new Exception(ExceptionDictionary.NotConnected);
             }
         }
     }
---
+++ models/Exceptions/ExceptionDictionary.cs
@@ -0,0 +1,5 @@
+namespace GitGrabber.Models {
+    public static class ExceptionDictionary {
+        public const string NotConnected = "You must be connected to perform an operation.";
+    }
+}
