7b29db7b4f777d7b1267ad61a90b94c96b093c58
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-06 15:56:48 -0300

    Update - Git Repo model

--- models/GitApiModels/GithubUserContent/GithubRepo.cs
+++ models/GitApiModels/GithubUserContent/GithubRepo.cs
@@ -2,9 +2,9 @@ namespace GitGrabber.Models {
     public class GithubRepo {
         public int id {get; set;}
         public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
+        public string name {get; set;}
+        public bool @private {get; set;} /* Info: private field of API requisition */
+        public string owner {get; set;} /* Info: Only the username will be fetched from the requisition */
         public string node_id {get; set;}
         public string node_id {get; set;}
         public string node_id {get; set;}
--- models/GitApiModels/GithubUserContent/GithubUser.cs
+++ models/GitApiModels/GithubUserContent/GithubUser.cs
@@ -15,40 +15,22 @@ namespace GitGrabber.Models {
         public string organizations_url {get; set;}
         public string repos_url {get; set;}
         public string events_url {get; set;}
-        
-        public string received_events_url {get; set;}
-        
-        public string type {get; set;}
-        
+        public string received_events_url {get; set;}      
+        public string type {get; set;} 
         public string site_admin {get; set;}
-        
         public string name {get; set;}
-        
         public string company {get; set;}
-        
         public string blog {get; set;}
-        
         public string location {get; set;}
-        
         public string email {get; set;}
-        
         public string hireable {get; set;}
-        
         public string bio {get; set;}
-        
         public string twitter_username {get; set;}
-        
         public int public_repos {get; set;}
-        
         public int public_gists {get; set;}
-        
         public int followers {get; set;}
-        
         public int following {get; set;}
-        
         public string created_at {get; set;}
-        
         public string updated_at {get; set;}
-
     }
 }
