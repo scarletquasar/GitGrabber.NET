a51a6528a8f43481b14ef41d0393d299f328353e
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-06 16:04:42 -0300

    Update - GithubRepo model

--- models/GitApiModels/GithubUserContent/GithubRepo.cs
+++ models/GitApiModels/GithubUserContent/GithubRepo.cs
@@ -5,35 +5,35 @@ namespace GitGrabber.Models {
         public string name {get; set;}
         public bool @private {get; set;} /* Info: private field of API requisition */
         public string owner {get; set;} /* Info: Only the username will be fetched from the requisition */
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
-        public string node_id {get; set;}
+        public string html_url {get; set;}
+        public string description {get; set;}
+        public bool fork {get; set;}
+        public string url {get; set;}
+        public string forks_url {get; set;}
+        public string keys_url {get; set;}
+        public string collaborators_url {get; set;}
+        public string teams_url {get; set;}
+        public string hooks_url {get; set;}
+        public string issue_events_url {get; set;}
+        public string events_url {get; set;}
+        public string assignees_url {get; set;}
+        public string branches_url {get; set;}
+        public string tags_url {get; set;}
+        public string blobs_url {get; set;}
+        public string git_tags_url {get; set;}
+        public string git_refs_url {get; set;}
+        public string trees_url {get; set;}
+        public string statuses_url {get; set;}
+        public string languages_url {get; set;}
+        public string stargazers_url {get; set;}
+        public string contributors_url {get; set;}
+        public string subscribers_url {get; set;}
+        public string subscription_url {get; set;}
+        public string commits_url {get; set;}
+        public string git_commits_url {get; set;}
+        public string comments_url {get; set;}
+        public string issue_comment_url {get; set;}
+        public string contents_url {get; set;}
         public string node_id {get; set;}
         public string node_id {get; set;}
         public string node_id {get; set;}
