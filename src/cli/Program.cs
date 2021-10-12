f6ba4ebac0976931df5196e48f81a64f6d50e5b7
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-12 16:58:06 -0300

    Update - Test CLI

--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -10,6 +10,9 @@ namespace cli
     {
         static void Main(string[] args)
         {
+            /* This console application is for testing purposes only and should not be confused 
+            with the main project (core) of the application which can be found under "src/core". */
+            
             GitGrabConnection GitConnection = new();
             GitConnection.Connect();
 
