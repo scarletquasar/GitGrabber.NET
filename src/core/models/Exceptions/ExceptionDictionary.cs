af78f23869bfc09e4d5a22b198e5f6f458c2e8e0
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-10-31 21:37:12 -0300

    Remove unused ExceptionDictionary

--- src/core/models/Exceptions/ExceptionDictionary.cs
+++
@@ -1,6 +0,0 @@
-namespace GitGrabber.Models {
-    public static class ExceptionDictionary {
-        public const string NotConnected = "You must be connected to perform an operation.";
-        public const string DynamicError = "Not handled error: ";
-    }
-}
